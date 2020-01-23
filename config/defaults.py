f1255d187bb63c5cacbfcaaad077e7362005b627
Zhao <alexzhao@cs.utexas.edu>
2020-01-23 13:23:16 -0600

    Initial commit

---
+++ config/defaults.py
@@ -0,0 +1,146 @@
+from yacs.config import CfgNode as CN
+
+# -----------------------------------------------------------------------------
+# Config definition
+# -----------------------------------------------------------------------------
+
+_C = CN()
+_C.DIR = "ckpt/ade20k-resnet50dilated-ppm_deepsup"
+
+# -----------------------------------------------------------------------------
+# Dataset
+# -----------------------------------------------------------------------------
+_C.DATASET = CN()
+_C.DATASET.name = "VOC"
+_C.DATASET.data_dir = "./data/pascal/VOCdevkit/VOC2012/"
+_C.DATASET.data_split = "trainaug"
+_C.DATASET.input_size = (256, 256)
+
+
+_C.DATASET.root_dataset = "./data/"
+_C.DATASET.list_train = "./data/training.odgt"
+_C.DATASET.list_val = "./data/validation.odgt"
+# multiscale train/test, size of short edge (int or tuple)
+_C.DATASET.imgSizes = (300, 375, 450, 525, 600)
+# maximum input image size of long edge
+_C.DATASET.imgMaxSize = 1000
+# maxmimum downsampling rate of the network
+_C.DATASET.padding_constant = 8
+# downsampling rate of the segmentation label
+_C.DATASET.segm_downsampling_rate = 8
+# randomly horizontally flip images when train/test
+_C.DATASET.random_flip = True
+_C.DATASET.ref_path = "./data/ref_training.txt"
+_C.DATASET.ref_val_path = "./data/ref_real_val.txt"
+_C.DATASET.ref_start = 0
+_C.DATASET.ref_end = 1
+_C.DATASET.ref_val_start = 0
+_C.DATASET.ref_val_end = 1
+_C.DATASET.random_pick = 1
+_C.DATASET.no_align = False
+_C.DATASET.zero_input_rgb = False
+_C.DATASET.zero_input_seg = False
+_C.DATASET.random_input_seg = False
+_C.DATASET.RGB_mask_combine_val = False
+
+# -----------------------------------------------------------------------------
+# Task
+# -----------------------------------------------------------------------------
+_C.TASK.n_ways = 1
+_C.TASK.n_shots = 1
+_C.TASK.n_queries = 1
+_C.TASK.fold_idx = 0
+
+# -----------------------------------------------------------------------------
+# Model
+# -----------------------------------------------------------------------------
+_C.MODEL = CN()
+# architecture of net_encoder
+_C.MODEL.arch_encoder = "resnet50dilated"
+# architecture of net_attention
+_C.MODEL.arch_attention = "attention"
+# architecture of net_decoder
+_C.MODEL.arch_decoder = "ppm_deepsup"
+# weights to finetune net_encoder
+_C.MODEL.weights_encoder = ""
+# weights to finetune net_decoder
+_C.MODEL.weights_decoder = ""
+_C.MODEL.weights_enc_query = ""
+_C.MODEL.weights_enc_memory = ""
+_C.MODEL.weights_att_query = ""
+_C.MODEL.weights_att_memory = ""
+# number of feature channels between encoder and decoder
+_C.MODEL.fc_dim = 2048
+_C.MODEL.att_fc_dim = 512
+_C.MODEL.mask_feat_downsample_rate = 1
+_C.MODEL.att_mat_downsample_rate = 1
+_C.MODEL.zero_memory = False
+_C.MODEL.zero_qval = False
+_C.MODEL.random_memory_bias = False
+_C.MODEL.random_memory_nobias = False
+_C.MODEL.random_scale = 1.0
+_C.MODEL.memory_encoder_noBN = False
+_C.MODEL.memory_encoder_arch = ""
+_C.MODEL.qval_qread_BN = False
+_C.MODEL.normalize_key = False
+_C.MODEL.p_scalar = 40.0
+_C.MODEL.memory_feature_aggregation = False
+_C.MODEL.memory_noLabel = False
+
+# -----------------------------------------------------------------------------
+# Training
+# -----------------------------------------------------------------------------
+_C.TRAIN = CN()
+_C.TRAIN.n_iters = 30000
+_C.TRAIN.n_batch = 2
+
+
+_C.TRAIN.batch_size_per_gpu = 2
+# epoch to start training. useful if continue from a checkpoint
+_C.TRAIN.start_iter = 0
+# iterations of each epoch (irrelevant to batch size)
+_C.TRAIN.epoch_iters = 5000
+
+_C.TRAIN.optim = "SGD"
+_C.TRAIN.lr_encoder = 0.02
+_C.TRAIN.lr_decoder = 0.02
+# power in poly to drop LR
+_C.TRAIN.lr_pow = 0.9
+# momentum for sgd, beta1 for adam
+_C.TRAIN.beta1 = 0.9
+# weights regularizer
+_C.TRAIN.weight_decay = 1e-4
+# the weighting of deep supervision loss
+_C.TRAIN.deep_sup_scale = 0.4
+# fix bn params, only under finetuning
+_C.TRAIN.fix_bn = False
+# number of data loading workers
+_C.TRAIN.workers = 16
+
+# frequency to display
+_C.TRAIN.disp_iter = 20
+# manual seed
+_C.TRAIN.seed = 304
+_C.TRAIN.save_freq = 10000
+
+# -----------------------------------------------------------------------------
+# Validation
+# -----------------------------------------------------------------------------
+_C.VAL = CN()
+# currently only supports 1
+_C.VAL.batch_size = 1
+# output visualization during validation
+_C.VAL.visualize = False
+# the checkpoint to evaluate on
+_C.VAL.checkpoint = "epoch_20.pth"
+
+# -----------------------------------------------------------------------------
+# Testing
+# -----------------------------------------------------------------------------
+_C.TEST = CN()
+# currently only supports 1
+_C.TEST.batch_size = 1
+# the checkpoint to test on
+_C.TEST.checkpoint = "epoch_20.pth"
+# folder to output visualization results
+_C.TEST.result = "./"
--- train.py
+++ train.py
@@ -2,6 +2,11 @@
 import os
 import shutil
 
+import random
+import argparse
+
+from config import cfg
+
 import torch
 import torch.nn as nn
 import torch.optim
@@ -13,8 +18,11 @@ from torchvision.transforms import Compose
 #from dataloaders.customized import voc_fewshot, coco_fewshot
 from dataloaders.transforms import RandomMirror, Resize, ToTensorNormalize
 from util.utils import set_seed, CLASS_LABELS
-from config import ex
+from utils_seg import AverageMeter, parse_devices, setup_logger
+from models import ModelBuilder, SegmentationAttentionSeparateModule
+from lib.nn import UserScatteredDataParallel, user_scattered_collate, patch_replication_callback
 import pdb
+
 
 def data_preprocess(sample_batched, cfg):
     feed_dict = {}
@@ -42,30 +50,164 @@ def data_preprocess(sample_batched, cfg):
 
     return feed_dict
 
+def checkpoint(nets, history, cfg, iter_idx):
+    print('Saving checkpoints...')
+    (net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit) = nets
 
-@ex.automain
-def main(_run, _config, _log):
-    if _run.observers:
-        os.makedirs(f'{_run.observers[0].dir}/snapshots', exist_ok=True)
-        for source_file, _ in _run.experiment_info['sources']:
-            os.makedirs(os.path.dirname(f'{_run.observers[0].dir}/source/{source_file}'),
-                        exist_ok=True)
-            _run.observers[0].save_file(source_file, f'source/{source_file}')
-        shutil.rmtree(f'{_run.observers[0].basedir}/_sources')
+    dict_enc_query = net_enc_query.state_dict()
+    dict_enc_memory = net_enc_memory.state_dict()
+    dict_att_query = net_att_query.state_dict()
+    dict_att_memory = net_att_memory.state_dict()
+    dict_decoder = net_decoder.state_dict()
+
+    torch.save(
+        history,
+        '{}/history_iter_{}.pth'.format(cfg.DIR, iter_idx))
+    torch.save(
+        dict_enc_query,
+        '{}/enc_query_iter_{}.pth'.format(cfg.DIR, iter_idx))
+    torch.save(
+        dict_enc_memory,
+        '{}/enc_memory_iter_{}.pth'.format(cfg.DIR, iter_idx))
+    torch.save(
+        dict_att_query,
+        '{}/att_query_iter_{}.pth'.format(cfg.DIR, iter_idx))
+    torch.save(
+        dict_att_memory,
+        '{}/att_memory_iter_{}.pth'.format(cfg.DIR, iter_idx))
+    torch.save(
+        dict_decoder,
+        '{}/decoder_iter_{}.pth'.format(cfg.DIR, iter_idx))
+
+def group_weight(module):
+    group_decay = []
+    group_no_decay = []
+    for m in module.modules():
+        if isinstance(m, nn.Linear):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, nn.modules.conv._ConvNd):
+            group_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+        elif isinstance(m, nn.modules.batchnorm._BatchNorm):
+            if m.weight is not None:
+                group_no_decay.append(m.weight)
+            if m.bias is not None:
+                group_no_decay.append(m.bias)
+
+    assert len(list(module.parameters())) == len(group_decay) + len(group_no_decay)
+    groups = [dict(params=group_decay), dict(params=group_no_decay, weight_decay=.0)]
+    return groups
+
+def create_optimizers(nets, cfg):
+    (net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit) = nets
+    optimizer_enc_query = torch.optim.SGD(
+        group_weight(net_enc_query),
+        lr=cfg.TRAIN.lr_encoder,
+        momentum=cfg.TRAIN.beta1,
+        weight_decay=cfg.TRAIN.weight_decay)
+    optimizer_enc_memory = torch.optim.SGD(
+        group_weight(net_enc_memory),
+        lr=cfg.TRAIN.lr_encoder,
+        momentum=cfg.TRAIN.beta1,
+        weight_decay=cfg.TRAIN.weight_decay)
+    optimizer_att_query = torch.optim.SGD(
+        group_weight(net_att_query),
+        lr=cfg.TRAIN.lr_encoder,
+        momentum=cfg.TRAIN.beta1,
+        weight_decay=cfg.TRAIN.weight_decay)
+    optimizer_att_memory = torch.optim.SGD(
+        group_weight(net_att_memory),
+        lr=cfg.TRAIN.lr_encoder,
+        momentum=cfg.TRAIN.beta1,
+        weight_decay=cfg.TRAIN.weight_decay)
+    optimizer_decoder = torch.optim.SGD(
+        group_weight(net_decoder),
+        lr=cfg.TRAIN.lr_decoder,
+        momentum=cfg.TRAIN.beta1,
+        weight_decay=cfg.TRAIN.weight_decay)
+    return (optimizer_enc_query, optimizer_enc_memory, optimizer_att_query, optimizer_att_memory, optimizer_decoder)
+
+def adjust_learning_rate(optimizers, cur_iter, cfg):
+    scale_running_lr = ((1. - float(cur_iter) / cfg.TRAIN.max_iters) ** cfg.TRAIN.lr_pow)
+    cfg.TRAIN.running_lr_encoder = cfg.TRAIN.lr_encoder * scale_running_lr
+    cfg.TRAIN.running_lr_decoder = cfg.TRAIN.lr_decoder * scale_running_lr
+
+    (optimizer_enc_query, optimizer_enc_memory, optimizer_att_query, optimizer_att_memory, optimizer_decoder) = optimizers
+    for param_group in optimizer_enc_query.param_groups:
+        param_group['lr'] = cfg.TRAIN.running_lr_encoder
+    for param_group in optimizer_enc_memory.param_groups:
+        param_group['lr'] = cfg.TRAIN.running_lr_encoder
+    for param_group in optimizer_att_query.param_groups:
+        param_group['lr'] = cfg.TRAIN.running_lr_encoder
+    for param_group in optimizer_att_memory.param_groups:
+        param_group['lr'] = cfg.TRAIN.running_lr_encoder
+    for param_group in optimizer_decoder.param_groups:
+        param_group['lr'] = cfg.TRAIN.running_lr_decoder
 
 
-    set_seed(_config['seed'])
-    cudnn.enabled = True
-    cudnn.benchmark = True
-    torch.cuda.set_device(device=_config['gpu_id'])
-    torch.set_num_threads(1)
+def main(cfg, gpus):
+    # Network Builders
+    print('###### Create model ######')
+    net_enc_query = ModelBuilder.build_encoder(
+        arch=cfg.MODEL.arch_encoder.lower(),
+        fc_dim=cfg.MODEL.fc_dim,
+        weights=cfg.MODEL.weights_enc_query)
+    if cfg.MODEL.memory_encoder_arch:
+        net_enc_memory = ModelBuilder.build_encoder_memory_separate(
+            arch=cfg.MODEL.memory_encoder_arch.lower(),
+            fc_dim=cfg.MODEL.fc_dim,
+            weights=cfg.MODEL.weights_enc_memory,
+            num_class=cfg.TASK.n_ways+1,
+            RGB_mask_combine_val=cfg.DATASET.RGB_mask_combine_val,
+            segm_downsampling_rate=cfg.DATASET.segm_downsampling_rate)
+    else:
+        if cfg.MODEL.memory_encoder_noBN:
+            net_enc_memory = ModelBuilder.build_encoder_memory_separate(
+                arch=cfg.MODEL.arch_encoder.lower()+'_nobn',
+                fc_dim=cfg.MODEL.fc_dim,
+                weights=cfg.MODEL.weights_enc_memory,
+                num_class=cfg.TASK.n_ways+1,
+                RGB_mask_combine_val=cfg.DATASET.RGB_mask_combine_val,
+                segm_downsampling_rate=cfg.DATASET.segm_downsampling_rate)
+        else:
+            net_enc_memory = ModelBuilder.build_encoder_memory_separate(
+                arch=cfg.MODEL.arch_encoder.lower(),
+                fc_dim=cfg.MODEL.fc_dim,
+                weights=cfg.MODEL.weights_enc_memory,
+                num_class=cfg.TASK.n_ways+1,
+                RGB_mask_combine_val=cfg.DATASET.RGB_mask_combine_val,
+                segm_downsampling_rate=cfg.DATASET.segm_downsampling_rate,
+                pretrained=cfg.memory_enc_pretrained)
+    net_att_query = ModelBuilder.build_att_query(
+        arch=cfg.MODEL.arch_attention,
+        fc_dim=cfg.MODEL.fc_dim,
+        weights=cfg.MODEL.weights_att_query)
+    net_att_memory = ModelBuilder.build_att_memory(
+        arch=cfg.MODEL.arch_attention,
+        fc_dim=cfg.MODEL.fc_dim,
+        att_fc_dim=cfg.MODEL.att_fc_dim,
+        weights=cfg.MODEL.weights_att_memory)
+    net_decoder = ModelBuilder.build_decoder(
+        arch=cfg.MODEL.arch_decoder.lower(),
+        fc_dim=cfg.MODEL.fc_dim,
+        num_class=cfg.TASK.n_ways+1,
+        weights=cfg.MODEL.weights_decoder)
+
+    crit = nn.NLLLoss(ignore_index=255)
+
+    if cfg.MODEL.arch_decoder.endswith('deepsup'):
+        segmentation_module = SegmentationAttentionSeparateModule(
+            net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit, cfg.TRAIN.deep_sup_scale, zero_memory=cfg.MODEL.zero_memory, random_memory_bias=cfg.MODEL.random_memory_bias, random_memory_nobias=cfg.MODEL.random_memory_nobias, random_scale=cfg.MODEL.random_scale, zero_qval=cfg.MODEL.zero_qval, qval_qread_BN=cfg.MODEL.qval_qread_BN, normalize_key=cfg.MODEL.normalize_key, p_scalar=cfg.MODEL.p_scalar, memory_feature_aggregation=cfg.MODEL.memory_feature_aggregation, memory_noLabel=cfg.MODEL.memory_noLabel, mask_feat_downsample_rate=cfg.MODEL.mask_feat_downsample_rate, att_mat_downsample_rate=cfg.MODEL.att_mat_downsample_rate)
+    else:
+        segmentation_module = SegmentationAttentionSeparateModule(
+            net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit, zero_memory=cfg.MODEL.zero_memory, random_memory_bias=cfg.MODEL.random_memory_bias, random_memory_nobias=cfg.MODEL.random_memory_nobias, random_scale=cfg.MODEL.random_scale, zero_qval=cfg.MODEL.zero_qval, qval_qread_BN=cfg.MODEL.qval_qread_BN, normalize_key=cfg.MODEL.normalize_key, p_scalar=cfg.MODEL.p_scalar, memory_feature_aggregation=cfg.MODEL.memory_feature_aggregation, memory_noLabel=cfg.MODEL.memory_noLabel, mask_feat_downsample_rate=cfg.MODEL.mask_feat_downsample_rate, att_mat_downsample_rate=cfg.MODEL.att_mat_downsample_rate)
 
 
-    _log.info('###### Create model ######')
-
-
-    _log.info('###### Load data ######')
-    data_name = _config['dataset']
+    print('###### Load data ######')
+    data_name = cfg.DATASET.name
     if data_name == 'VOC':
         from dataloaders.customized import voc_fewshot
         make_data = voc_fewshot
@@ -74,39 +216,180 @@ def main(_run, _config, _log):
         make_data = coco_fewshot
     else:
         raise ValueError('Wrong config for dataset!')
-    labels = CLASS_LABELS[data_name][_config['label_sets']]
-    transforms = Compose([Resize(size=_config['input_size']),
+    labels = CLASS_LABELS[data_name][cfg.TASK.fold_idx]
+    transforms = Compose([Resize(size=cfg.DATASET.input_size),
                           RandomMirror()])
     dataset = make_data(
-        base_dir=_config['path'][data_name]['data_dir'],
-        split=_config['path'][data_name]['data_split'],
+        base_dir=cfg.DATASET.data_dir,
+        split=cfg.DATASET.data_split,
         transforms=transforms,
         to_tensor=ToTensorNormalize(),
         labels=labels,
-        max_iters=_config['n_steps'] * _config['batch_size'],
-        #n_ways=_config['task']['n_ways'],
-        n_ways=2,
-        n_shots=_config['task']['n_shots'],
-        n_queries=_config['task']['n_queries']
+        max_iters=cfg.TRAIN.n_iters * cfg.TRAIN.n_batch,
+        n_ways=cfg.TASK.n_ways,
+        n_shots=cfg.TASK.n_shots,
+        n_queries=cfg.TASK.n_queries
     )
     trainloader = DataLoader(
         dataset,
-        #batch_size=_config['batch_size'],
-        batch_size=2,
+        batch_size=cfg.TRAIN.n_batch,
         shuffle=True,
-        num_workers=1,
+        num_workers=4,
         pin_memory=True,
         drop_last=True
     )
 
-    _log.info('###### Set optimizer ######')
+    segmentation_module = UserScatteredDataParallel(
+        segmentation_module,
+        device_ids=gpus)
+    # For sync bn
+    patch_replication_callback(segmentation_module)
+    #segmentation_module = nn.DataParallel(segmentation_module, device_ids=gpus)
+    segmentation_module.cuda()
 
-    i_iter = 0
-    log_loss = {'loss': 0, 'align_loss': 0}
-    _log.info('###### Training ######')
+    # Set up optimizers
+    nets = (net_enc_query, net_enc_memory, net_att_query, net_att_memory, net_decoder, crit)
+    optimizers = create_optimizers(nets, cfg)
+
+    batch_time = AverageMeter()
+    data_time = AverageMeter()
+    ave_total_loss = AverageMeter()
+    ave_acc = AverageMeter()
+
+    history = {'train': {'iter': [], 'loss': [], 'acc': []}}
+
+    segmentation_module.train(not cfg.TRAIN.fix_bn)
+
+    # main loop
+    tic = time.time()
+
+    print('###### Training ######')
     for i_iter, sample_batched in enumerate(trainloader):
         # Prepare input
-        feed_dict = data_preprocess(sample_batched)
+        feed_dict = data_preprocess(sample_batched, cfg)
 
-        # Forward and Backward
-        pdb.set_trace()
+        data_time.update(time.time() - tic)
+        segmentation_module.zero_grad()
+
+        # adjust learning rate
+        adjust_learning_rate(optimizers, i_iter, cfg)
+
+        # forward pass
+        #print(batch_data)
+        loss, acc = segmentation_module(feed_dict)
+        loss = loss.mean()
+        acc = acc.mean()
+
+        # Backward
+        loss.backward()
+        for optimizer in optimizers:
+            optimizer.step()
+
+        # measure elapsed time
+        batch_time.update(time.time() - tic)
+        tic = time.time()
+
+        # update average loss and acc
+        ave_total_loss.update(loss.data.item())
+        ave_acc.update(acc.data.item()*100)
+
+        # calculate accuracy, and display
+        if i % cfg.TRAIN.disp_iter == 0:
+            print('Iter: [{}][{}/{}], Time: {:.2f}, Data: {:.2f}, '
+                  'lr_encoder: {:.6f}, lr_decoder: {:.6f}, '
+                  'Accuracy: {:4.2f}, Loss: {:.6f}'
+                  .format(i_iter, i_iter, cfg.TRAIN.n_iters,
+                          batch_time.average(), data_time.average(),
+                          cfg.TRAIN.running_lr_encoder, cfg.TRAIN.running_lr_decoder,
+                          ave_acc.average(), ave_total_loss.average()))
+
+            history['train']['iter'].append(i_iter)
+            history['train']['loss'].append(loss.data.item())
+            history['train']['acc'].append(acc.data.item())
+
+        if (i_iter+1) % cfg.TRAIN.save_freq == 0:
+            checkpoint(nets, history, cfg, i_iter+1)
+
+    print('Training Done!')
+
+
+
+if __name__ == '__main__':
+    assert LooseVersion(torch.__version__) >= LooseVersion('0.4.0'), \
+        'PyTorch>=0.4.0 is required'
+
+    parser = argparse.ArgumentParser(
+        description="PyTorch Semantic Segmentation Training"
+    )
+    parser.add_argument(
+        "--cfg",
+        default="config/ade20k-resnet50dilated-ppm_deepsup.yaml",
+        metavar="FILE",
+        help="path to config file",
+        type=str,
+    )
+    parser.add_argument(
+        "--gpus",
+        default="0-3",
+        help="gpus to use, e.g. 0-3 or 0,1,2,3"
+    )
+    parser.add_argument(
+        "opts",
+        help="Modify config options using the command-line",
+        default=None,
+        nargs=argparse.REMAINDER,
+    )
+    parser.add_argument(
+        "--memory_enc_pretrained",
+        action='store_true',
+        help="use a pretrained memory encoder",
+    )
+    args = parser.parse_args()
+
+    cfg.merge_from_file(args.cfg)
+    cfg.merge_from_list(args.opts)
+    cfg.memory_enc_pretrained = args.memory_enc_pretrained
+    # cfg.freeze()
+
+    logger = setup_logger(distributed_rank=0)   # TODO
+    logger.info("Loaded configuration file {}".format(args.cfg))
+    logger.info("Running with config:\n{}".format(cfg))
+
+    # Output directory
+    if not os.path.isdir(cfg.DIR):
+        os.makedirs(cfg.DIR)
+    logger.info("Outputing checkpoints to: {}".format(cfg.DIR))
+    with open(os.path.join(cfg.DIR, 'config.yaml'), 'w') as f:
+        f.write("{}".format(cfg))
+
+    # Start from checkpoint
+    if cfg.TRAIN.start_iter > 0:
+        cfg.MODEL.weights_enc_query = os.path.join(
+            cfg.DIR, 'enc_query_iter_{}.pth'.format(cfg.TRAIN.start_iter))
+        cfg.MODEL.weights_enc_memory = os.path.join(
+            cfg.DIR, 'enc_memory_iter_{}.pth'.format(cfg.TRAIN.start_iter))
+        cfg.MODEL.weights_att_query = os.path.join(
+            cfg.DIR, 'att_query_iter_{}.pth'.format(cfg.TRAIN.start_iter))
+        cfg.MODEL.weights_att_memory = os.path.join(
+            cfg.DIR, 'att_memory_iter_{}.pth'.format(cfg.TRAIN.start_iter))
+        cfg.MODEL.weights_decoder = os.path.join(
+            cfg.DIR, 'decoder_iter_{}.pth'.format(cfg.TRAIN.start_iter))
+        assert os.path.exists(cfg.MODEL.weights_enc_query) and os.path.exists(cfg.MODEL.weights_enc_memory) and \
+            os.path.exists(cfg.MODEL.weights_att_query) and os.path.exists(cfg.MODEL.weights_att_memory) and \
+            os.path.exists(cfg.MODEL.weights_decoder), "checkpoint does not exitst!"
+
+    # Parse gpu ids
+    gpus = parse_devices(args.gpus)
+    gpus = [x.replace('gpu', '') for x in gpus]
+    gpus = [int(x) for x in gpus]
+    num_gpus = len(gpus)
+    cfg.TRAIN.batch_size = num_gpus * cfg.TRAIN.batch_size_per_gpu
+
+    cfg.TRAIN.max_iters = cfg.TRAIN.n_iters
+    cfg.TRAIN.running_lr_encoder = cfg.TRAIN.lr_encoder
+    cfg.TRAIN.running_lr_decoder = cfg.TRAIN.lr_decoder
+
+    random.seed(cfg.TRAIN.seed)
+    torch.manual_seed(cfg.TRAIN.seed)
+
+    main(cfg, gpus)

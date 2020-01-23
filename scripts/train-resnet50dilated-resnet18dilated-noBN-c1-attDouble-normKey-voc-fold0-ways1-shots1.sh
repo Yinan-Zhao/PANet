cc8522b9d3ea448b40a3c8c47732a344672b51ed
Zhao <alexzhao@cs.utexas.edu>
2020-01-23 13:25:08 -0600

    Initial commit

---
+++ scripts/train-resnet50dilated-resnet18dilated-noBN-c1-attDouble-normKey-voc-fold0-ways1-shots1.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+python -u train.py --gpus 0 --cfg config/resnet50dilated-resnet18dilated-noBN-c1-attDouble-normKey-voc-fold0-ways1-shots1.yaml \
+2>&1 | tee ./output/resnet50dilated-resnet18dilated-noBN-c1-attDouble-normKey-voc-fold0-ways1-shots1.log
+

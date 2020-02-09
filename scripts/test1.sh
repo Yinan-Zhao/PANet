3ac3ed67b688907d51e47d7b6e0ec94083c2ddd9
Zhao <alexzhao@cs.utexas.edu>
2020-02-09 11:55:08 -0600

    Initial commit

--- scripts/test1.sh
+++ scripts/test1.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-python test.py --gpu 0 --data_split trainaug --fold_idx 1 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold1.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 1 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold1.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 1 --n_runs 3 --eval_att_voting --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold1-evalAttVoting.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 1 --n_runs 3 --eval_att_voting --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold1-evalAttVoting.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 0 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold0.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 0 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold0.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 2 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold2.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 2 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-lr2e3-trainaug-testfold2.log
 
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 1 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold1.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 1 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold1.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 1 --n_runs 3 --eval_att_voting --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold1-evalAttVoting.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 1 --n_runs 3 --eval_att_voting --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold1-evalAttVoting.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 0 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold0.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 0 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold0.log
 
-python test.py --gpu 0 --data_split trainaug --fold_idx 2 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold2.log
+python test.py --gpu 1 --data_split trainaug --fold_idx 2 --n_runs 3 --cfg config/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1.yaml --checkpoint iter_best.pth 2>&1 | tee output_test/resnet50deeplab-globalPool-attDouble-c1-voc-fold1-ways1-shots1-trainaug-testfold2.log
 
